b63c2d03bfc3f6d4f1342a4aaff0aaed45fe7e60
murgeo13 <166647253+murgeo13@users.noreply.github.com>
2024-04-16 00:35:24 +0300

    Update cls_del.sh

--- cls_del.sh
+++ cls_del.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "script is started"
-# берём переменные из конфиг файла
+# load variables
 source variables.txt
 mkdir -p ${log_dir}
 mkdir -p ${ref_dir}
@@ -9,45 +9,45 @@ mkdir -p ${out_dir}
 bowtie2-build ${ref} ${indexes} &>${log_dir}/bowtie2-build.log
 echo "indexes are made"
 
-# картируем чтения
+# map reads
 bowtie2 -p 10 -x ${indexes} -1 ${in_F} -2 ${in_R} -S ${sam} 2>${log_dir}/bowtie2.log
-echo "reads are mapped"
-# конвертация файла
-echo "конвертация файла" 1>> ${log_dir}/samtools.log
+echo "reads are maped"
+# convert file
+echo "convertation ${sam} to ${bam}" 1>> ${log_dir}/samtools.log
 samtools sort -o ${bam} ${sam} 2>>${log_dir}/samtools.log
 
-# индексируем bam файл
-echo "индексируем bam файл" 1>> ${log_dir}/samtools.log
+# index bam
+echo "indexing of ${bam}" 1>> ${log_dir}/samtools.log
 samtools index ${bam} 2>${log_dir}/bam_index.log
-echo "bam is indexed"
+echo "${bam} is indexed"
 
-# Получение чтений, картированных на вашу хромосому
-echo "Получение чтений, картированных на хромосому "${chr} 1>> ${log_dir}/samtools.log
+# get reads mapped to chromosome
+echo "getting reads mapped to ${chr}" 1>> ${log_dir}/samtools.log
 samtools view -h -bS ${bam} ${chr} 1> ${bam_on_chr} 2>>${log_dir}/samtools.log
-# индексируем правильный bam файл
-echo "индексируем правильный bam файл" 1>> ${log_dir}/samtools.log
+# index bam
+echo "indexing of ${bam_on_chr}" 1>> ${log_dir}/samtools.log
 samtools index ${bam_on_chr}
 samtools mpileup ${bam_on_chr} -A --output-extra FLAG,POS,RNEXT,PNEXT -a -o ${out_dir}/all_cover.txt
 
-#используем delly
-echo "Поиск перестроек с помощью Delly"
+# Delly
+echo "discovering of SVs with Delly"
 delly call -o ${delly_out_bcf} -g ${ref} ${bam_on_chr} &>>${log_dir}/delly.log
 bcftools view ${delly_out_bcf} > ${delly_out_vcf} 2>>${log_dir}/delly.log
 echo "Delly is done"
 
-#статистика picard
-echo "Собрать статистику по размеру вставки между чтениями"
+# statistics with picard
+echo "CollectInsertSizeMetrics"
 picard CollectInsertSizeMetrics -I ${bam_on_chr} -O ${TLEN_txt} -H ${TLEN_pdf} -M 0.5 2>> ${log_dir}/picard.log
 grep -v ^# ${TLEN_txt} | sed -n "2,3p" | datamash transpose --output-delimiter="=" 1> ${TLEN_var}
 
 source ${TLEN_var}
-echo "Получение чтений, картированных на хромосому ${chr} c большим TLEN" 1>> ${log_dir}/samtools.log
+echo "getting reads mapped to ${chr} with large TLEN" 1>> ${log_dir}/samtools.log
 samtools view --input-fmt-option filter="tlen>${MEDIAN_INSERT_SIZE}+3*${MEDIAN_ABSOLUTE_DEVIATION} || tlen<-${MEDIAN_INSERT_SIZE}-3*${MEDIAN_ABSOLUTE_DEVIATION}" \
 -bS ${bam_on_chr} 1> ${bam_filtered} 2>>${log_dir}/samtools.log
 
 samtools mpileup ${bam_filtered} -A --output-extra FLAG,POS,RNEXT,PNEXT -a -o ${out_dir}/filtered_cover.txt
 
-#python
+# python
 python ./cls_del/main.py --chr ${chr} --chrcov ${out_dir}/filtered_cover.txt --chrcov-total ${out_dir}/all_cover.txt \
 --median ${MEDIAN_INSERT_SIZE} --sd ${MEDIAN_ABSOLUTE_DEVIATION} \
 --out ./python_out_new --save-temp-files --debug  2>>${log_dir}/python.log
